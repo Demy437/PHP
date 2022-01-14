7b97d48ff80445a69a2174dec5b603290628e68e
Demy437 <97069175@st.deltion.nl>
2022-01-14 10:46:45 +0100

    idk

--- multiauth/routes/api.php
+++ multiauth/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,23 +15,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-//===============================USERS=================================
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-//========================LOGIN/REGISTER==================
-// Route::group(['middleware' => ['cors', 'json.response']], function () {
-
-//     // public routes
-//     Route::post('login', [RegisteredUserController::class, 'login']);
-//     Route::post('register', [RegisteredUserController::class, 'register']);
-    
-    
-// });
-
-//=============================CRUD=============================
-Route::group(['middleware' => ['auth', 'role:admin']], function() { 
-    Route::resource('articles', ArticleController::class);
-
 });
